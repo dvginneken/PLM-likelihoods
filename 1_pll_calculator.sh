4f6a2d7ebc8a2ca309b35632d7fc3e79a33f2bda
dvanginneken <d.van.ginneken@hotmail.com>
2024-04-25 14:45:32 +0200

    first commit

---
+++ 1_pll_calculator.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=pll_calculator
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=100gb
+#SBATCH --time=100:00:00
+#SBATCH --output=log/pll_calculator.out
+#SBATCH --error=log/pll_calculator.error
+
+
+cd /hpc/dla_lti/dvanginneken/PLM-likelihoods/scripts
+modes=("cdr3_only" "full_VDJ" "cdr3_from_VDJ")
+#datasets=("OVA_V7" "horns2020a__VDJ_RAW")
+datasets="COVID"
+for mode in "${modes[@]}"
+do
+    for data in "${datasets[@]}"
+    do
+        python3 pll_calculator.py --dataset=$data --mode=$mode
+        echo "done $data $mode"
+    done
+done
+echo "finished"
