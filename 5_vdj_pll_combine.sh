e50ab4ea355ebcac05af1fc5a7f22e4e63e6b6c8
dvanginneken <d.van.ginneken@hotmail.com>
2024-04-30 14:45:12 +0200

    Combine likelihood and VDJ object

---
+++ 5_vdj_pll_combine.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=10:00:00
+#SBATCH --mem=100G
+#SBATCH --job-name=VDJ_PLL
+#SBATCH --error=log/VDJ_pll_combine.error
+#SBATCH --output=log/VDJ_pll_combine.out
+
+cd /hpc/dla_lti/dvanginneken/PLM-likelihoods/scripts
+datasets=("OVA_V7" "horns2020a__VDJ_RAW")
+for data in "${datasets[@]}"
+  do
+      Rscript vdj_pll_combine.R $data
+  done
+echo "finished"
+
