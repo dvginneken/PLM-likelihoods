5c74483fea55ba9d0bd621354fc4dae462f98787
dvanginneken <d.van.ginneken@hotmail.com>
2024-05-23 12:33:21 +0200

    update

--- 8_evovelocity_adata.sh
+++ 8_evovelocity_adata.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-#SBATCH --job-name=evovelocity_adata
+#SBATCH --job-name=evovelocity
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=100gb
 #SBATCH --time=100:00:00
-#SBATCH --output=log/evovelocity_adata.out
-#SBATCH --error=log/evovelocity_adata.error
+#SBATCH --output=log/evovelocity.out
+#SBATCH --error=log/evovelocity.error
 
-
+export TORCH_HOME=/hpc/dla_lti/dvanginneken/cache/
 cd /hpc/dla_lti/dvanginneken/PLM-likelihoods/scripts
-#datasets=("OVA_V7" "horns2020a__VDJ_RAW" "Bruhn")
-#for data in "${datasets[@]}"
-#do
-#    python3 evo_velo_adata_maker_karlis.py --dataset=$data
-#    echo "done $data"
-#done
-#echo "finished"
+#datasets=("OVA_V7" "horns2020a__VDJ_RAW" "Bruhn" "Bieberich")
+datasets="Bruhn"
+for data in "${datasets[@]}"
+do
+    python3 evo_velo_adata_maker_karlis.py --dataset=$data --model="esm" --sample_id="Bruhn" --input_source="full_VDJ" --color="c_gene"
+    echo "done $data"
+done
+echo "finished"
 
-python3 evo_velo_adata_maker_karlis.py --dataset_file="../data/Bruhn/VDJ/Bruhn/embeddings/full_VDJ/embeddings_esm.csv.gzip" --model="ESM"
+
--- scripts/evo_velo_adata_maker_karlis.py
+++ scripts/evo_velo_adata_maker_karlis.py
@@ -10,51 +10,78 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--dataset_file', help='Path to the dataset file containing embeddings')
-parser.add_argument("-m", "--model", help="Choose a model: ESM, ablang, protbert, sapiens")
-args = parser.parse_args()
-dataset_file = args.dataset_file
-model = args.model
+parser.add_argument("-m", "--model", help="Choose a model: esm, ablang, protbert, sapiens")
+parser.add_argument("-s", "--sample_id")
+parser.add_argument("-i", "--input_source", help="cdr3_only or full_VDJ")
+parser.add_argument("-d", "--dataset")
+parser.add_argument("-c", "--color", help="Choose from: SHM_count or c_gene")
 
-embeddings_df = pd.read_csv(dataset_file, compression="gzip")
-embeddings_df = embeddings_df.dropna(subset=['full_sequence'])
-embeddings_df['full_sequence'] = embeddings_df['full_sequence'].astype(str)
-#Sequences from embeddings and other metadata
-sequences = embeddings_df['full_sequence']
-sequences = [str(seq) for seq in sequences]
-vdj_cgene = [str(gene) for gene in embeddings_df['c_gene']]
-sample_ids =[str(sample_id) for sample_id in embeddings_df['sample_id']]
-shm_count = [float(count) for count in embeddings_df["SHM_count"]]
-embedding_cols = [col for col in embeddings_df.columns if col.startswith('dim')]
-metadata_cols = [col for col in embeddings_df.columns if col not in embedding_cols]
+args = parser.parse_args()
+model = args.model
+sample_id = args.sample_id
+input_source = args.input_source
+dataset = args.dataset
+color = args.color
+
+#Get embeddings, substring the barcode, only keep heavy chain, and remove NA values from dimensions and barcodes
+embedding_file = os.path.join("..","data",dataset,"VDJ",sample_id,"embeddings",input_source,f"embeddings_{model}.csv.gzip")
+embeddings_df = pd.read_csv(embedding_file, compression="gzip")
+embeddings_df['barcode'] = embeddings_df['barcode'].str[:-2] 
+embeddings_df = embeddings_df[embeddings_df['chain'] == "IGH"]
+embeddings_df = embeddings_df.dropna(subset=["dim_0"])
+embeddings_df = embeddings_df.dropna(subset=["barcode"])
+
+#Get VDJ information for a specific sample
+vdj_file = os.path.join("..","data",dataset,"vdj_evolike_combine.csv")
+vdj_df = pd.read_csv(vdj_file)
+vdj_df = vdj_df[vdj_df['sample_id'] == sample_id]
+
+#Match embeddings and vdj metadata
+df_merged = pd.merge(embeddings_df, vdj_df, on='barcode', how='inner')
+
+#Get the sequences
+if input_source == "full_VDJ":
+    sequences = [str(gene) for gene in df_merged['VDJ_sequence_aa_trimmed']]
+if input_source == "cdr3_only":
+    sequences = [str(gene) for gene in df_merged['VDJ_cdr3_aa']]
+
+#Get other metadata to plot
+vdj_cgene = [str(gene) for gene in df_merged['c_gene']]
+shm_count = [float(count) for count in df_merged["SHM_count"]]
+
+#Get embedding columns and metadata columns
+embedding_cols = [col for col in df_merged.columns if col.startswith('dim')]
+metadata_cols = [col for col in df_merged.columns if col not in embedding_cols]
 
 #Create an AnnData object
-adata = anndata.AnnData(embeddings_df[embedding_cols])
+adata = anndata.AnnData(df_merged[embedding_cols])
 
 #Add sequence and metadata information to .obs slot of AnnData object
 adata.obs['seq'] = sequences
 adata.obs['c_gene'] = vdj_cgene
-adata.obs['sample_id'] = sample_ids
+#adata.obs['sample_id'] = sample_ids
 adata.obs["SHM_count"] = shm_count
 
 #Construct sequence similarity network
 evo.pp.neighbors(adata)
 sc.tl.umap(adata)
 basis = "umap"
-if model  == "ESM":
+if model  == "esm":
     evo.tl.velocity_graph(adata)
 else:
     evo.tl.velocity_graph(adata, model_name = model)
 if 'model' in adata.uns:
     del adata.uns['model']
+
 #Embed network and velocities in two-dimensions
 evo.tl.velocity_embedding(adata, basis = basis)
+
 #Save the processed AnnData object to a file
-output_file = "../processed_data.h5ad" 
+output_file = os.path.join("..","data",dataset,"evo-velocity",f"adata_{sample_id}_{input_source}_{model}.h5ad")
 adata.write(output_file)
 print(f"Processed data saved to {output_file}")
+
 # Plot your data using the color mapping (jgene or SHM_count), make sure data is categorical
-color="SHM_count"
 ax = evo.pl.velocity_embedding_stream(adata, color=color, legend_loc="right margin", show=False)
-save_path = f"evo_velo_embedding_stream_{color}_{model}.png"
+save_path = os.path.join("..","data",dataset,"evo-velocity",f"embedding_{color}_{sample_id}_{input_source}_{model}.png")
 plt.savefig(save_path, bbox_inches="tight")
